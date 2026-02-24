5ebfc0abb0d20a97c1f9984fdac390e31a1a3246
subham pradhan <sp8768824@gmail.com>
2026-02-24 16:41:48 +0530

    Add files via upload
    My portfolio

--- Portfolio.java
+++ Portfolio.java
@@ -13,35 +13,50 @@ public class Portfolio extends HttpServlet {
         out.println("<html>");
         out.println("<head>");
         out.println("<title>My Portfolio</title>");
-        out.println("<style>");
-        out.println("body{font-family:Arial; background-color:#f4f4f4;}");
-        out.println(".container{width:70%; margin:auto; background:white; padding:20px;}");
-        out.println("h1{color:darkblue;}");
-        out.println("</style>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<div class='container'>");
+
+        out.println("<center>");
         out.println("<h1>My Portfolio</h1>");
         out.println("<hr>");
+
+        out.println("<img src='photo.jpg' width='150' height='150'><br><br>");
+
         out.println("<h2>Personal Details</h2>");
         out.println("<p><b>Name:</b> Subham Pradhan</p>");
         out.println("<p><b>Course:</b> B.Tech CSE</p>");
         out.println("<p><b>Email:</b> subham@gmail.com</p>");
+
         out.println("<h2>Skills</h2>");
-        out.println("<ul>");
-        out.println("<li>Java,C,PYTHON</li>");
-        out.println("<li>Servlet</li>");
-        out.println("<li>HTML, CSS</li>");
-        out.println("<li>DBMS</li>");
-        out.println("</ul>");
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("<th>Skill</th>");
+        out.println("<th>Level</th>");
+        out.println("</tr>");
+
+        out.println("<tr><td>Java</td><td>Intermediate</td></tr>");
+        out.println("<tr><td>C</td><td>Intermediate</td></tr>");
+        out.println("<tr><td>Python</td><td>Beginner</td></tr>");
+        out.println("<tr><td>Servlet</td><td>Intermediate</td></tr>");
+        out.println("<tr><td>HTML & CSS</td><td>Intermediate</td></tr>");
+        out.println("<tr><td>DBMS</td><td>Intermediate</td></tr>");
+        out.println("</table><br>");
+
         out.println("<h2>Projects</h2>");
-        out.println("<ul>");
-        out.println("<li>CureCart - Online Pharmacy System</li>");
-        out.println("<li>smart quizz portal</li>");
-        out.println("</ul>");
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("<th>Project Name</th>");
+        out.println("<th>Description</th>");
+        out.println("</tr>");
+
+        out.println("<tr><td>CureCart</td><td>Online Pharmacy System</td></tr>");
+        out.println("<tr><td>Smart Quiz Portal</td><td>Web-based Quiz using Servlet</td></tr>");
+        out.println("</table><br>");
+
         out.println("<h2>Career Objective</h2>");
         out.println("<p>To become a skilled software developer and contribute to innovative projects.</p>");
-        out.println("</div>");
+
+        out.println("</center>");
         out.println("</body>");
         out.println("</html>");
     }
